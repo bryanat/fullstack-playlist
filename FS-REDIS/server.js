2c89f7fd3153f8824beff371330673b739fd58a5
bryanat <boyettbryan@gmail.com>
2022-08-30 22:56:09 -0400

    Add song router to express app

--- FS-REDIS/server.js
+++ FS-REDIS/server.js
@@ -1,9 +1,15 @@
 import 'dotenv/config'
 import express from 'express'
 
+// import song router
+import { router as songRouter } from './routers/song-router.js'
+
 // create an express app using json middleware
 const app = new express()
 app.use(express.json())
+
+// add song router to Express app
+app.use('/song', songRouter)
 
 // start the server
 app.listen(8085)
